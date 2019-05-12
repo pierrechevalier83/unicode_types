7b7e8cf81219e7bceeb7a3953cfc3b460404b3c4
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-05-12 03:57:25 +0100

    Wrap generated src files into a library

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+/// Mapping of all unicode characters to rust types.
+///
+/// This mapping was automatically generated from the latest Unicode
+/// Character Database by the `unicode_gen` crate.
+///
+/// Each unicode Block is represented by a module of the appropriate name.
+/// In this module, one module named constants contains a constant for each
+/// character litteral in the block.
+/// In addition to this, an enum represents the block and each character
+/// is represented by a variant of this enum.
+///
+/// Disclaimer:
+/// Unicode Character Database
+/// Date: 2019-04-29, 23:54:00 GMT [KW]
+/// © 2019 Unicode®, Inc.
+/// Unicode and the Unicode Logo are registered trademarks of Unicode,
+/// Inc. in the U.S. and other countries.
+/// For terms of use, see http://www.unicode.org/terms_of_use.html
+mod generated;
+pub use generated::*;
